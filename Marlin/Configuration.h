8d32be3645a7b2853499d59e4c42cfb6516321b4
alejandrofdiaz <alejandrofdiaz@gmail.com>
2016-10-20 23:25:18 +0200

    Redefinir el offset del sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -470,7 +470,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -25     // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -29     // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.75  // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.55  // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
